bbff2990fc6a704ef04d0dcbb5417f606c3cbb17
MD. Sirajul Islam <106694080+sirajul26@users.noreply.github.com>
2023-06-14 13:18:18 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-javascript:(function () { var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () { eruda.init() } })();
+javascript:(function () {
+  var script = document.createElement('script');
+  script.src="//cdn.jsdelivr.net/npm/eruda";
+  document.body.appendChild(script);
+  script.onload = function () {
+    eruda.init();
+  };
+})();
+
